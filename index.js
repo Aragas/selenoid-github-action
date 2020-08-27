0d40a193424d959afe99277fdb1d131e5dea9c4c
Vitaly Mikhailov <micvitalij@yandex.ru>
2020-08-28 00:58:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { execSync } = require("child_process");
 
-console.log(`## DOWNLOADING CM AND STARTING SELENOID`);
+console.log(`## DOWNLOADING CM`);
 
-execSync(`curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid start`)
+execSync(`curl -s https://aerokube.com/cm/bash | bash`)
+
+console.log(`## STARTING SELENOID`);
+
+execSync(`./cm selenoid start`)
 
 console.log(`## DOWNLOADING CM AND STARTING SELENOID FINISHED`);
